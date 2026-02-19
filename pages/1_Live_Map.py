599d3ea0a9255ce1e1a466aec448a44701a0684c
fastgreenracing <benjamin.t.dewitt@gmail.com>
2026-02-19 12:17:58 -0800

    Update 1_Live_Map.py

--- pages/1_Live_Map.py
+++ pages/1_Live_Map.py
@@ -57,7 +57,7 @@ with st.sidebar:
 st.subheader("🗺️ Course Overview & Key Points")
 st.markdown('<div class="map-container">', unsafe_allow_html=True)
 # Replace the URL below with your actual Google My Maps embed link
-st.components.v1.html('<iframe src="https://www.google.com/maps/d/u/0/embed?mid=YOUR_MAP_ID_HERE" width="100%" height="600" style="border:none;"></iframe>', height=600)
+st.components.v1.html('<iframe src="https://www.google.com/maps/d/embed?mid=1UOQuxT6lSaKGXm2wmjVzeFwVuORY8Vk&hl=en&ehbc=2E312F" width="640" height="480"></iframe>', height=600)
 st.markdown('</div>', unsafe_allow_html=True)
 
 st.divider()
